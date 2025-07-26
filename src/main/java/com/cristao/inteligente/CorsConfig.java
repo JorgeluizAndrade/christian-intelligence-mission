b26be6843048e5c5c16d25555c2d3cd03e05b8cf
jorge luiz <luizj1718@gmail.com>
2025-07-26 09:41:19 -0300

    adding cors

---
+++ src/main/java/com/cristao/inteligente/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.cristao.inteligente;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply to all endpoints
+                .allowedOrigins("http://localhost:3000") // Allow requests from this origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specified HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Allow sending of cookies and authentication headers
+    }
+
+}
